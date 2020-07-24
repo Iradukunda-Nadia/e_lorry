47ef5375c565f2a58fd132e61505ba2467b63c11
Iradukunda-Nadia <esthernadia70@gmail.com>
2020-07-24 10:04:11 +0300

    changes2

---
+++ lib/admin/Tickets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tickets extends StatefulWidget {
+  @override
+  _TicketsState createState() => _TicketsState();
+}
+
+class _TicketsState extends State<Tickets> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Tickets'),),
+    );
+  }
+}
