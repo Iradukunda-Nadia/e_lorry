fed576c2528ac6d3ca08e926dc915093cb0eb98c
Iradukunda-Nadia <esthernadia70@gmail.com>
2020-11-22 17:56:03 +0300

    changes2

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,25 @@ Future<void> main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var email = prefs.getString('email');
   print(email);
-  runApp(MaterialApp(home: email == null ? LoginScreen() : Logged()));
+  runApp(MaterialApp(
+      title: 'E-lorry',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+
+      routes: <String, WidgetBuilder>{
+        '/LoginScreen': (BuildContext context) => new LoginScreen()
+      },
+      home: email == null ? LoginScreen() : Logged()));
 }
 
 class MyApp extends StatelessWidget {
